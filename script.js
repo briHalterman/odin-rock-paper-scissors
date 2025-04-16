7d96910cec0149012f28b95a5c9000004fc84b6b
Brittany Halterman <halterma@gmail.com>
2025-04-16 15:03:25 -0400

    Write the logic to get the computer choice
    Create a new function named `getComputerChoice` and write the code so
    that getComputerChoice will randomly return one of the following string
    values: “rock”, “paper” or “scissors” using an if/else statement.

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 // script.js
-console.log('Hello World');
+
+// write a function that randomly returns “rock”, “paper” or “scissors”
+
+// Create a new function named getComputerChoice
+const getComputerChoice = () => {
+  // Write the code so that getComputerChoice will randomly return one of the following string values: “rock”, “paper” or “scissors”
+
+  let computerChoice = '';
+
+  // get random number
+  randomNumber = Math.floor(Math.random() * 3);
+  // use if/else to return a value based on the randomly generated number
+  if (randomNumber === 0) {
+    computerChoice = 'rock';
+  } else if (randomNumber === 1) {
+    computerChoice = 'paper';
+  } else if (randomNumber === 2) {
+    computerChoice = 'scissors';
+  }
+
+  return computerChoice;
+};
+
+console.log(getComputerChoice());
