9b6cccae78315f28828e8db81d3df6ed11333f75
Brittany Halterman <halterma@gmail.com>
2025-04-16 15:48:08 -0400

    Write the logic to play a single round
    The game will be played round by round. Write a function that takes the
    human and computer player choices as arguments, plays a single round,
    increments the round winner’s score and logs a winner announcement.

--- script.js
+++ script.js
@@ -28,9 +28,39 @@ const getHumanChoice = () => {
 
   // prompt the user to input choice
   humanChoice = prompt('Rock? Paper? Scissors? Shoot:');
+  return humanChoice;
 };
 
 // Create two new variables named humanScore and computerScore in the global scope
 // Initialize those variables with the value of 0
 let humanScore = 0;
 let computerScore = 0;
+
+// Write the logic to play a single round
+
+// Create a new function named playRound.
+// Define two parameters for playRound: humanChoice and computerChoice.
+const playRound = (humanChoice, computerChoice) => {
+  // Make your function’s humanChoice parameter case-insensitive so that players can input “rock”, “ROCK”, “RocK”, or other variations.
+  caseInsensitiveHumanChoice = humanChoice.toLowerCase();
+
+  // Write the code for your playRound function to console.log a string value representing the round winner, such as: “You lose! Paper beats Rock”.
+  if (humanChoice === computerChoice) {
+    console.log("It's a tie!");
+  } else if (
+    (caseInsensitiveHumanChoice === 'rock' &&
+      computerChoice === 'scissors') ||
+    (caseInsensitiveHumanChoice === 'paper' &&
+      computerChoice === 'rock') ||
+    (caseInsensitiveHumanChoice === 'scissors' &&
+      computerChoice === 'paper')
+  ) {
+    humanScore++;
+    console.log(`You win! ${humanChoice} beats ${computerChoice}!`);
+  } else {
+    computerScore++;
+    console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+  }
+};
+
+playRound(getHumanChoice(), getComputerChoice());
